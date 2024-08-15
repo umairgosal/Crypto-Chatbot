d15bc6390cde334118b385875f5c5e6c1778c39d
umairgosal <umairgosal123@gmail.com>
2024-08-15 18:03:01 +0500

    testing deployment

--- crypto-chatbot/app/page.tsx
+++ crypto-chatbot/app/page.tsx
@@ -37,6 +37,7 @@ export default function Home() {
           height={37}
           priority
         />
+        <h1>hi this is gosal</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
